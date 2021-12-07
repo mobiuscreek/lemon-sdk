413e1f8fe7112c3ed30be9bb06babbb8b6f43ff5
mobiuscreek <36884713+mobiuscreek@users.noreply.github.com>
2021-12-07 15:16:47 +0000

    Set up skeleton for refactoring auth/client api

--- lemon/client/auth.py
+++ lemon/client/auth.py
@@ -1,18 +1,32 @@
+from core.account import Account
+from core.market import MarketData
+from core.orders import Order
+from core.strategy import IStrategy
+
+class LemonClient():
+    def __init__:
+        self.token = token
 
 
-
-def key_path() -> str:
-    import os
-    credential_file = os.environ.get("LEMON_CREDENTIALS")
-    if credential_file is not None:
-        return credential_file
-    else:
-        raise ValueError("LEMON_CREDENTIALS not set in ENVIRONMENT_VARIABLES")
-
-
-
-def credentials():
+@classmethod
+   def from_yaml():
     import yaml
     with open(key_path(), "r") as yamlfile:
         return yaml.load(yamlfile, Loader=yaml.FullLoader)["lemon-markets"]['api_key']
-    
+
+
+@classmethod
+   def from_env():
+    import os
+    credential_file = os.environ.get("LEMON_CREDENTIALS")
+
+
+  def account_api(self) -> Account:
+
+  def market_api(self) -> MarketData:
+
+  def orders_api(self): -> Order:
+
+  def strategy_api(self): -> IStrategy:
+
+ 
