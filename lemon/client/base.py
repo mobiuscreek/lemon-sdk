8f653380852da28617dafe2167c61837f2ca9743
mobiuscreek <36884713+mobiuscreek@users.noreply.github.com>
2021-12-08 11:29:59 +0000

    Add separate function calls for each API via client

--- lemon/client/base.py
+++ lemon/client/base.py
@@ -1,7 +1,7 @@
-#from lemon.core.account import Account
-#from lemon.core.market import MarketData
-#from lemon.core.orders import Order
-#from lemon.core.strategy import Strategy
+from lemon.core.account import Account
+from lemon.core.market import MarketData
+from lemon.core.orders import Order
+from lemon.core.strategy import Strategy
 
 class LemonBase():
     def __init__(self, token: str):
@@ -15,11 +15,21 @@ class LemonBase():
             return cls(token=yaml.load(yamlfile, Loader=yaml.FullLoader)["lemon-markets"]['api_key'])
 
 
-
     @classmethod
     def from_env(cls):
         import os
         credential_file = os.environ.get("LEMON_CREDENTIALS")
 
 
+    def account_api(self):
+        return Account(self._token)
+
+    def order_api(self, *args, **kwargs):
+        return Order(self._token, *args, **kwargs)
+
+    def market_api(self):
+        return MarketData(self._token)
+    
+    def strategy_api(self):
+        return Strategy(self._token)
 
